dd557254b3688d893f7a629da0421310b3c80303
Kori Roys <kori@koriroys.com>
2015-06-24 18:55:05 +0200

    it kinda works

--- lib/bush_viper.rb
+++ lib/bush_viper.rb
@@ -1,5 +1,35 @@
 require "bush_viper/version"
+require "faraday"
+require "pry"
 
 module BushViper
-  # Your code goes here...
+  module Mendeley
+    class API
+
+      BASE_URL = "https://api.mendeley.com/"
+
+      def initialize(token)
+        self.token = token
+      end
+
+      def documents
+        get("documents")
+      end
+
+      private
+
+      def connection
+        @connection ||= Faraday.new(url: BASE_URL)
+      end
+
+      def get(url)
+        connection.get do |request|
+          request.url url
+          request.headers["Authorization"] = "Bearer #{token}"
+        end.body
+      end
+
+      attr_accessor :token
+    end
+  end
 end
---
+++ test.rb
@@ -0,0 +1,8 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require "bush_viper"
+
+m = BushViper::Mendeley::API.new("MSwxNDM1MTY0NTU0NzIwLDMzNTAwOTI2MSw3MTIsYWxsLCwsOTRmMzA2NzM2MmU0MTM5NGRiMTM1ZmNhYmFiMTBiNGUzZDk0MS14eGEsRGZfdzNMeWR2ZDVNRkU1TmlDTUtWemtsODVN")
+
+puts m.documents
