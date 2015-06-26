0f0656bc8d4b7f519aafda5f8217118ea1597bc0
Kori Roys <kori@koriroys.com>
2015-06-26 11:00:14 +0200

    more tests

--- test/lib/first_test.rb
+++ test/lib/first_test.rb
@@ -4,4 +4,8 @@ describe BushViper::Mendeley::API do
   it "defines the base url constant" do
     BushViper::Mendeley::API::BASE_URL.must_equal "https://api.mendeley.com/"
   end
+
+  it "initializes with an access_token" do
+    BushViper::Mendeley::API.new("token").send(:token).must_equal "token"
+  end
 end
